89d624d1cf2e0a45cb411167bf69d0888c483db0
cidelx <ctetteh1@terpmail.umd.edu>
2025-12-21 21:54:56 -0500

    new

--- backend/supabase.js
+++ backend/supabase.js
@@ -3,6 +3,6 @@ const {
 } = require("@supabase/supabase-js");
 
 const supabaseUrl = "https://waifbhduwgnxpzjimlws.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhaWZiaGR1d2dueHB6amltbHdzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NjIyNTQ2MiwiZXhwIjoyMDgxODAxNDYyfQ.dRxqh6CaPDZttCvwjcA3nGdAJk5yRvYh_vMrdq0wL-4";
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = { supabase };
