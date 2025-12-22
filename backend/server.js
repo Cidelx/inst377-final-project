6f7a8bdf27049e3ad18aa044b51d91098a8d94c6
cidelx <ctetteh1@terpmail.umd.edu>
2025-12-22 01:29:46 -0500

    add root package.json

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,7 @@ app.post("/api/reviews", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 module.exports = app;
-
